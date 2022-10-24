6d3935a7d1fb27d84d12aea2b3d2125b24e7a229
brooks <brooks.houck@gmail.com>
2022-10-24 22:21:29 +0000

    moved env var config

--- main.tf
+++ main.tf
@@ -16,9 +16,3 @@ module "key_pair" {
     separator = "."
   }
 
-variable "AWS_ACCESS_KEY_ID" {
-    type = string
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-    type = string
-}
--- provider.tf
+++ provider.tf
@@ -14,6 +14,14 @@ terraform {
   }
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+}
+
+
 provider "aws" {
   region = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
