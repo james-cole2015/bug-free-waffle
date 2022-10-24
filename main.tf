59d9d7712cf0595c4d2e3580b5f76656198a80a2
brooks <brooks.houck@gmail.com>
2022-10-24 15:55:11 +0000

    test

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ module "key_pair" {
   source  = "terraform-aws-modules/key-pair/aws"
   version = "2.0.0"
 
-  key_name   = "new-tf_key-9e76544"
+  key_name   = "new-tf_key-9e7333333555iii6544"
   public_key = tls_private_key.rsa.public_key_openssh
 }
