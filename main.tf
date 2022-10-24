15012497a8ab00b19292405b28fc0eb2a4c3a986
james-cole2015 <brooks.houck@gmail.com>
2022-10-24 17:42:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,11 @@ module "key_pair" {
   source  = "terraform-aws-modules/key-pair/aws"
   version = "2.0.0"
 
-  key_name   = "new-tf_key-9e7333333555iii6544"
+  key_name   = $"{random_pet.pet_1.id}-key"
   public_key = tls_private_key.rsa.public_key_openssh
 }
+  
+  resource "random_pet" "pet_1" {
+    length = 2
+    seperator = "."
+  }
